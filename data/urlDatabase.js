dc46ad84640f98eb2986d92f410bf83e77eb9dbe
Tharsikan <thar.navaratnam@gmail.com>
2021-11-10 19:20:55 +0000

    modularize urlDatabase

---
+++ data/urlDatabase.js
@@ -0,0 +1,12 @@
+const urlDatabase = {
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "testuser123"
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "testuserabc"
+  }
+};
+
+module.exports = urlDatabase;
