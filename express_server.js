d94ccdc6d8ff15e8e865e83c217e6a981e28df2e
Tharsikan <thar.navaratnam@gmail.com>
2021-11-10 19:11:33 +0000

    modularize helper functions

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const cookieSession = require('cookie-session')
+const cookieSession = require('cookie-session');
+const { generateRandomString, getUserByEmail, getURLsByUserId } = require("./helpers")
 const cookieParser = require('cookie-parser');
 const bcrypt = require("bcrypt");
 const PORT = 8080; // default port 8080
@@ -40,53 +41,6 @@ const users = {
     email: "test@test.com",
     password: "abcdefgh"
   }
-}
-
-const generateRandomChar = () => {
-  const randomNumber = Math.floor(Math.random() * (57 - 48 + 1)) + 48;
-  const randomCapitalLetter = Math.floor(Math.random() * (90 - 65 + 1)) + 65;
-  const randomLowercaseLetter = Math.floor(Math.random() * (122 - 97 + 1)) + 97;
-  const charType = Math.floor(Math.random() * 3) + 1;
-
-  let charCode;
-  if (charType === 1) charCode = randomNumber;
-  if (charType === 2) charCode = randomCapitalLetter;
-  if (charType === 3) charCode = randomLowercaseLetter;
-
-  return String.fromCharCode(charCode);
-}
-
-const generateRandomString = (length) => {
-  let str = "";
-
-  while (str.length < length) {
-    str += generateRandomChar();
-  }
-
-  return str;
-}
-
-const getUserByEmail = (email, users) => {
-  const vals = Object.values(users);
-  let user;
-
-  vals.forEach((value) => {
-    if (value.email === email) user = users[value.id];
-  });
-
-  return user;
-}
-
-const getURLsByUserId = (id, urlDatabase) => {
-  const usersURLs = {}
-
-  for (const shortURL in urlDatabase) {
-    if (urlDatabase[shortURL].userID === id) {
-      usersURLs[shortURL] = { ...urlDatabase[shortURL] }
-    }
-  };
-
-  return usersURLs;
 }
 
 app.get("/", (req, res) => {
---
+++ helpers.js
@@ -0,0 +1,48 @@
+const generateRandomChar = () => {
+  const randomNumber = Math.floor(Math.random() * (57 - 48 + 1)) + 48;
+  const randomCapitalLetter = Math.floor(Math.random() * (90 - 65 + 1)) + 65;
+  const randomLowercaseLetter = Math.floor(Math.random() * (122 - 97 + 1)) + 97;
+  const charType = Math.floor(Math.random() * 3) + 1;
+
+  let charCode;
+  if (charType === 1) charCode = randomNumber;
+  if (charType === 2) charCode = randomCapitalLetter;
+  if (charType === 3) charCode = randomLowercaseLetter;
+
+  return String.fromCharCode(charCode);
+}
+
+const generateRandomString = (length) => {
+  let str = "";
+
+  while (str.length < length) {
+    str += generateRandomChar();
+  }
+
+  return str;
+}
+
+const getUserByEmail = (email, users) => {
+  const vals = Object.values(users);
+  let user;
+
+  vals.forEach((value) => {
+    if (value.email === email) user = users[value.id];
+  });
+
+  return user;
+}
+
+const getURLsByUserId = (id, urlDatabase) => {
+  const usersURLs = {}
+
+  for (const shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userID === id) {
+      usersURLs[shortURL] = { ...urlDatabase[shortURL] }
+    }
+  };
+
+  return usersURLs;
+}
+
+module.exports = { generateRandomString, getUserByEmail, getURLsByUserId }
