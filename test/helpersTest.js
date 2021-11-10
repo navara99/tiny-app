e0fbf10fae58d69dc0b82bc974e63f7eae4b9d74
Tharsikan <thar.navaratnam@gmail.com>
2021-11-10 20:03:59 +0000

    test getUserByEmail for invalid email case

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,24 +1,33 @@
 const { assert } = require('chai');
-
 const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
-    password: "purple-monkey-dinosaur"
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "uniquepassword12124"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
-    password: "dishwasher-funk"
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "aDifferentuniquepass998_"
   }
 };
 
-describe('getUserByEmail', function() {
-  it('should return a user with valid email', function() {
+describe('getUserByEmail', function () {
+  it('should return a user with valid email', function () {
     const user = getUserByEmail("user@example.com", testUsers)
-    const expectedUserID = "userRandomID";
-    assert(user, expectedUserID);
+    console.log(user);
+    const expectedUser = {
+      id: "userRandomID",
+      email: "user@example.com",
+      password: "uniquepassword12124"
+    };
+    assert.deepEqual(user, expectedUser);
+  });
+
+  it('should return undefined with an invalid email', function () {
+    const user = getUserByEmail("user100@example.com", testUsers);
+    assert.isUndefined(user);
   });
 });
