0ffbe1d2ff54551a707180bca219098946cf057d
Tharsikan <thar.navaratnam@gmail.com>
2021-11-10 19:21:22 +0000

    modularize usersData

---
+++ data/usersData.js
@@ -0,0 +1,19 @@
+const users = {
+  "testuser123": {
+    id: "testuser123",
+    email: "user@example.com",
+    password: "88888888"
+  },
+  "testuserabc": {
+    id: "testuserabc",
+    email: "user2@example.com",
+    password: "22222222"
+  },
+  "E02a9OV5xz": {
+    id: "E02a9OV5xz",
+    email: "test@test.com",
+    password: "abcdefgh"
+  }
+};
+
+module.exports = users;
