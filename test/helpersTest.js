09a9bc2314264ca08d7942011d4bdadee765a0e2
Tharsikan <thar.navaratnam@gmail.com>
2021-11-10 20:52:24 +0000

    add test for getURLsByUserId when input is valid

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,5 +1,5 @@
 const { assert } = require('chai');
-const { getUserByEmail } = require('../helpers.js');
+const { getUserByEmail, getURLsByUserId } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
@@ -14,10 +14,24 @@ const testUsers = {
   }
 };
 
+const urlDatabase = {
+  "b2xVn2": {
+    longURL: "https://ca.nba.com/?gr=www",
+    userID: "userRandomID"
+  },
+  "sag34X": {
+    longURL: "https://github.com/",
+    userID: "userRandomID"
+  },
+  "9sm5xK": {
+    longURL: "https://www.udemy.com/",
+    userID: "user2RandomID"
+  }
+};
+
 describe('getUserByEmail', function () {
   it('should return a user with valid email', function () {
     const user = getUserByEmail("user@example.com", testUsers)
-    console.log(user);
     const expectedUser = {
       id: "userRandomID",
       email: "user@example.com",
@@ -32,13 +46,27 @@ describe('getUserByEmail', function () {
   });
 
   it('should return undefined if first parameter is falsy', function () {
-    const user = getUserByEmail("" , testUsers);
+    const user = getUserByEmail(null, testUsers);
     assert.isUndefined(user);
   });
 
   it('should return undefined if second parameter is falsy', function () {
-    const user = getUserByEmail("" , testUsers);
+    const user = getUserByEmail("user@example.com", null);
     assert.isUndefined(user);
   });
+
+});
+
+describe('getURLsByUserId', function () {
+  it('should return a list of URLs belonging to the user when the input is a valid user id', function () {
+    const URLs = getURLsByUserId("userRandomID", urlDatabase);
+    const expectedURLs = {
+      b2xVn2: { longURL: 'https://ca.nba.com/?gr=www', userID: 'userRandomID' },
+      sag34X: { longURL: 'https://github.com/', userID: 'userRandomID' }
+    }
+    
+    assert.deepEqual(URLs, expectedURLs);
+});
+
 
 });
