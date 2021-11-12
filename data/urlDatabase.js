313d4a31b2a76236ad8d3bcf0485c9fc882326a6
Tharsikan <thar.navaratnam@gmail.com>
2021-11-12 01:37:53 +0000

    add more demo urls

--- data/urlDatabase.js
+++ data/urlDatabase.js
@@ -1,15 +1,40 @@
+const { getTodaysDate } = require("../helpers");
+
 const urlDatabase = {
   "b2xVn2": {
     longURL: "http://www.lighthouselabs.ca",
-    userID: "testuser123",
+    userID: "testuser",
     logs: [], // Keep track of every visit in this array,
-    visitors: [] // keep track of every visitor's id
+    visitors: [], // keep track of every visitor's id,
+    date: getTodaysDate()
   },
   "9sm5xK": {
     longURL: "http://www.google.com",
-    userID: "testuserabc",
+    userID: "testuser",
     logs: [],
-    visitors: []
+    visitors: [],
+    date: getTodaysDate()
+  },
+  "jkI45a": {
+    longURL: "https://www.freecodecamp.org/",
+    userID: "testuser2",
+    logs: [],
+    visitors: [],
+    date: getTodaysDate()
+  },
+  "jfa231": {
+    longURL: "https://www.npmjs.com/",
+    userID: "testuser",
+    logs: [],
+    visitors: [],
+    date: getTodaysDate()
+  },
+  "sgga2l": {
+    longURL: "https://stackoverflow.com/",
+    userID: "testuser",
+    logs: [],
+    visitors: [],
+    date: getTodaysDate()
   }
 };
 
