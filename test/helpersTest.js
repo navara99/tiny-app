e34276bc6a5da55256b766268ad109a7b235a4bd
Tharsikan <thar.navaratnam@gmail.com>
2021-11-12 02:08:33 +0000

    remove console.log

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -29,9 +29,9 @@ const urlDatabase = {
   }
 };
 
-describe('getUserByEmail', function () {
-  it('should return a user with valid email', function () {
-    const user = getUserByEmail("user@example.com", testUsers)
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers);
     const expectedUser = {
       id: "userRandomID",
       email: "user@example.com",
@@ -40,73 +40,72 @@ describe('getUserByEmail', function () {
     assert.deepEqual(user, expectedUser);
   });
 
-  it('should return undefined with an invalid email', function () {
+  it('should return undefined with an invalid email', function() {
     const user = getUserByEmail("user100@example.com", testUsers);
     assert.isUndefined(user);
   });
 
-  it('should return undefined if first parameter is falsy', function () {
+  it('should return undefined if first parameter is falsy', function() {
     const user = getUserByEmail(null, testUsers);
     assert.isUndefined(user);
   });
 
-  it('should return undefined if second parameter is falsy', function () {
+  it('should return undefined if second parameter is falsy', function() {
     const user = getUserByEmail("user@example.com", null);
     assert.isUndefined(user);
   });
 
 });
 
-describe('getURLsByUserId', function () {
-  it('should return a list of URLs belonging to the user when the input is a valid user id', function () {
+describe('getURLsByUserId', function() {
+  it('should return a list of URLs belonging to the user when the input is a valid user id', function() {
     const URLs = getURLsByUserId("userRandomID", urlDatabase);
     const expectedURLs = {
       b2xVn2: { longURL: 'https://ca.nba.com/?gr=www', userID: 'userRandomID' },
       sag34X: { longURL: 'https://github.com/', userID: 'userRandomID' }
-    }
+    };
 
     assert.deepEqual(URLs, expectedURLs);
   });
 
-  it('should return an empty object if there are no URLs associated with the user id', function () {
+  it('should return an empty object if there are no URLs associated with the user id', function() {
     const URLs = getURLsByUserId("user123", urlDatabase);
     assert.deepEqual(URLs, {});
   });
 
-  it('should return an empty object if the url database argument is an empty object', function () {
+  it('should return an empty object if the url database argument is an empty object', function() {
     const URLs = getURLsByUserId("userRandomID", {});
     assert.deepEqual(URLs, {});
   });
 
-  it('should return an empty object if first parameter is falsy', function () {
+  it('should return an empty object if first parameter is falsy', function() {
     const URLs = getURLsByUserId(null, urlDatabase);
     assert.deepEqual(URLs, {});
   });
 
-  it('should return an empty object if second parameter is falsy', function () {
+  it('should return an empty object if second parameter is falsy', function() {
     const URLs = getURLsByUserId("userRandomID", null);
     assert.deepEqual(URLs, {});
   });
 
 });
 
-describe('generateRandomString', function () {
+describe('generateRandomString', function() {
 
-  it('should return a string with length 6 when the argument is 6', function () {
+  it('should return a string with length 6 when the argument is 6', function() {
     const strLength = generateRandomString(6).length;
     const expectedStrLength = 6;
     assert.equal(strLength, expectedStrLength);
   });
 
-  it('should return an empty string when the argument is 0', function () {
+  it('should return an empty string when the argument is 0', function() {
     const str = generateRandomString(0);
     const expectedStr = "";
     assert.equal(str, expectedStr);
   });
 
-  it('should return an empty string when the argument is not a number', function () {
+  it('should return an empty string when the argument is not a number', function() {
     const str = generateRandomString("bob");
-    console.log(str);
     const expectedStr = "";
     assert.equal(str, expectedStr);
   });
