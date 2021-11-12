27889fd5f63a89d2257b023675178e19d01cccef
Tharsikan <thar.navaratnam@gmail.com>
2021-11-12 01:38:08 +0000

    add more demo users

--- data/usersData.js
+++ data/usersData.js
@@ -4,6 +4,11 @@ const users = {
     email: "user@example.com",
     hashedPassword: "$2b$10$5o4LIQQop2rc/HeuCq.S9.Mqo55VH8IGehKFot7jIY7a24sEZraKq" // Password is "watermelon"
   },
+  "testuser2": {
+    id: "testuser2",
+    email: "user2@example.com",
+    hashedPassword: "$2b$10$l8OKF./.8JkmFfB2jBdQSe35X/edS2GLR7b0Lk40nOHHFBox3TTVy" // Password is "123"
+  },
 };
 
 module.exports = users;
